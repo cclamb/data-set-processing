7e8385eb64c75719ceb4e9e534282c6f4cff69b2
cclamb <cclamb@sandia.gov>
2015-07-08 16:38:08 -0600

    trying to increase performance. Currently getting hung up on transferring large amounts of data between processes, seemingly.

--- bin/process.py
+++ bin/process.py
@@ -5,19 +5,23 @@ __author__ = 'cclamb'
 import ipdb
 import os
 import multiprocessing
+import numpy
 import cPickle
 import gzip
 
+from bitarray import bitarray
 from PIL import Image
 
 IMAGE_HEIGHT = 128
 IMAGE_WIDTH = 128
 MAX_VALUE = 255
 
-ROOT = 'data/mnist-sd19/BY_CLASS/'
+ROOT = 'data/small-sets/BY_CLASS/'
 DIRECTORY = '30'
 FULL_DIRECTORY = 'data/mnist-sd19/BY_CLASS/30/'
 FILENAME = 'data/mnist-sd19/BY_CLASS/30/HSF_0_00001-00250.png'
+
+ARCHIVE_NAME = 'mnist-sd19.pkl.gz'
 
 logger = None
 
@@ -62,13 +66,11 @@ def process_file(directory, filename):
         for i in xrange(IMAGE_HEIGHT):
             for j in xrange(IMAGE_WIDTH):
                 data.append(pixels[i, j] / MAX_VALUE)
-        images.append(data)
+        # images.append(tuple(data))
+        images.append(bitarray(data))
     return images
 
 def process_images(directory, clazz):
-    global logger
-    logger = Logger('./log-' + str(os.getpid()))
-    logger.create()
     logger.write('starting...')
 
     tr, te, va = extract_files(directory)
@@ -78,33 +80,41 @@ def process_images(directory, clazz):
     tei = [process_file(directory, file) for file in te]
     vai = [process_file(directory, file) for file in va]
 
-    # Flatten the generated lists
+    # # Flatten the generated lists
     training_set = [image for sublist in tri for image in sublist]
     testing_set = [image for sublist in tei for image in sublist]
     validation_set = [image for sublist in vai for image in sublist]
 
     logger.write('finished.')
-    logger.close()
 
     return training_set, validation_set, testing_set, clazz
-    # return ['training_set'], ['validation_set'], ['testing_set'], clazz
+    #return ['training_set'], ['validation_set'], ['testing_set'], clazz
 
 def image_processor(directory, clazz, queue):
+    global logger
+    logger = Logger('./log-' + str(os.getpid()))
+    logger.create()
     tr, va, te, clazz = process_images(directory, clazz)
+    logger.write('inserting into queue...')
     queue.put((clazz, (tr, va, te)))
+    logger.write('inserted into queue and terminating process.')
+    logger.close()
 
 def package(results):
     tr_clazz_list, va_clazz_list, te_clazz_list = [], [], []
     tr_list, va_list, te_list = [], [], []
     for result in results:
         clazz, (tr, va, te) = result
-        tr_clazz_list.extend([clazz] * len(tr))
-        va_clazz_list.extend([clazz] * len(va))
-        te_clazz_list.extend([clazz] * len(te))
+        tr_clazz_list.extend([int(clazz)] * len(tr))
+        va_clazz_list.extend([int(clazz)] * len(va))
+        te_clazz_list.extend([int(clazz)] * len(te))
         tr_list.extend(tr)
         va_list.extend(va)
         te_list.extend(te)
-    return ((tr_list, tr_clazz_list), (va_list, va_clazz_list), (te_list, te_clazz_list))
+    tr_tuple = (tuple(tr_list), tuple(tuple(tr_clazz_list)))
+    va_tuple = (tuple(va_list), tuple(tuple(va_clazz_list)))
+    te_tuple = (tuple(te_list), tuple(tuple(te_clazz_list)))
+    return tr_tuple, va_tuple, te_tuple
 
 
 def process():
@@ -118,17 +128,22 @@ def process():
             for clazz in clazzes
     ]
 
+    print 'Starting jobs...'
     for job in jobs:
         job.start()
 
+    print 'Joining jobs...'
     for job in jobs:
         job.join()
 
+    print 'packaging results...'
     return package([result_queue.get() for c in clazzes])
 
 if __name__ == '__main__':
-    #results = process()
-    tr, va, te, clazz = process_images(ROOT + DIRECTORY + '/', '30')
-    package = package([(clazz, (tr, va, te))])
-    with gzip.open('mnist-sd19.pkl', 'wb') as f:
+    package = process()
+    # tr, va, te, clazz = process_images(ROOT + DIRECTORY + '/', '30')
+    # package = package([(clazz, (tr, va, te))])
+    # ipdb.set_trace()
+    print 'dumping to archive.'
+    with gzip.open(ARCHIVE_NAME, 'wb') as f:
         cPickle.dump(package, f)
