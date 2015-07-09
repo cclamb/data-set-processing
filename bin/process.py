862144e3d00c2d422ae7c3c02632b140c15032d8
cclamb <cclamb@sandia.gov>
2015-07-09 15:33:40 -0600

    cleanup for single process work

--- bin/process.py
+++ bin/process.py
@@ -2,10 +2,9 @@
 
 __author__ = 'cclamb'
 
-import ipdb
+
 import os
-import multiprocessing
-import numpy
+import timeit
 import cPickle
 import gzip
 
@@ -17,27 +16,7 @@ IMAGE_WIDTH = 128
 MAX_VALUE = 255
 
 ROOT = 'data/mnist-sd19/BY_CLASS/'
-DIRECTORY = '30'
-FULL_DIRECTORY = 'data/mnist-sd19/BY_CLASS/30/'
-FILENAME = 'data/mnist-sd19/BY_CLASS/30/HSF_0_00001-00250.png'
-
 ARCHIVE_NAME = 'mnist-sd19.pkl.gz'
-
-logger = None
-
-
-class Logger(object):
-    def __init__(self, filename):
-        self.filename = filename
-
-    def create(self):
-        self.logfile = open(self.filename, 'w', 0)
-
-    def write(self, msg):
-        self.logfile.write(msg + '\n')
-
-    def close(self):
-        self.logfile.close()
 
 
 def extract_files(directory):
@@ -56,8 +35,7 @@ def process_file(directory, filename):
     record = Image.open(directory + filename)
     record_size = record.size;
     images = []
-    #print 'processing %s' % directory + filename
-    logger.write('processing %s' % directory + filename)
+    print 'processing %s' % directory + filename
     for i in xrange(record_size[1]/IMAGE_HEIGHT):
         box = (0, IMAGE_HEIGHT * i, IMAGE_WIDTH, IMAGE_HEIGHT * (i + 1))
         crop = record.crop(box)
@@ -66,15 +44,11 @@ def process_file(directory, filename):
         for i in xrange(IMAGE_HEIGHT):
             for j in xrange(IMAGE_WIDTH):
                 data.append(pixels[i, j] / MAX_VALUE)
-        # images.append(tuple(data))
         images.append(bitarray(data))
     return images
 
 def process_images(directory, clazz):
-    global logger
-    logger = Logger('./log-' + str(os.getpid()))
-    logger.create()
-    logger.write('starting...')
+    print 'starting...'
 
     tr, te, va = extract_files(directory)
 
@@ -88,20 +62,18 @@ def process_images(directory, clazz):
     testing_set = [image for sublist in tei for image in sublist]
     validation_set = [image for sublist in vai for image in sublist]
 
-    logger.write('finished.')
-    logger.close()
+    print 'finished.'
 
     return training_set, validation_set, testing_set, clazz
-    #return ['training_set'], ['validation_set'], ['testing_set'], clazz
 
 def package(results):
     tr_clazz_list, va_clazz_list, te_clazz_list = [], [], []
     tr_list, va_list, te_list = [], [], []
     for result in results:
         clazz, (tr, va, te) = result
-        tr_clazz_list.extend([int(clazz)] * len(tr))
-        va_clazz_list.extend([int(clazz)] * len(va))
-        te_clazz_list.extend([int(clazz)] * len(te))
+        tr_clazz_list.extend([hex(int(clazz, 16))] * len(tr))
+        va_clazz_list.extend([hex(int(clazz, 16))] * len(va))
+        te_clazz_list.extend([hex(int(clazz, 16))] * len(te))
         tr_list.extend(tr)
         va_list.extend(va)
         te_list.extend(te)
@@ -112,12 +84,12 @@ def package(results):
 
 
 def process():
-    # clazzes = ['30', '31']
     clazzes = [o for o in os.listdir(ROOT) if os.path.isdir(ROOT + o)]
-    ipdb.set_trace()
     data = []
+    start_time = timeit.default_timer()
     for c in clazzes:
         tr, va, te, clazz = process_images(ROOT + c + '/', c)
+        print '...elapsed time: %f' % (timeit.default_timer() - start_time)
         data.append((clazz, (tr, va, te)))
 
     return package(data)
